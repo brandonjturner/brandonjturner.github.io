cf46e0ae850347f4d3932fa2e2a1848404410f12
Brandon Turner <brandon88@gmail.com>
2018-11-21 00:55:15 -0800

    push

--- src/About.js
+++ src/About.js
@@ -1,15 +1,11 @@
 import React, { Component } from "react";
- 
 class Home extends Component {
   render() {
     return (
       <div>
 
-        
-
       </div>
     );
   }
 }
- 
 export default Home;
--- src/Main.js
+++ src/Main.js
@@ -13,21 +13,16 @@ class Main extends Component {
       <HashRouter>
         <div>
           <div>
-          <div class="hname" style= {{  }}>
           <h1>Brandon J Turner</h1>
-          </div>
-          <div class="job" style= {{ }}>
-          <h2>Full Stack Developer</h2>
-          </div>
           </div>
           <div class="social" style= {{ float:"right" }}>
             <a href="https://www.linkedin.com/in/brandonjturner" class="fa fa-linkedin"></a>
             <a href="https://www.github.com/brandonjturner" class="fa fa-github"></a>
           </div>
           <ul className="navlinks">
-            <li><NavLink exact to="/">About</NavLink></li>
-            <li><NavLink to="/portfolio">Portfolio</NavLink></li>
-            <li><NavLink to="/contact">Contact</NavLink></li>
+            <li><NavLink exact to="/"><h3>About</h3></NavLink></li>
+            <li><NavLink to="/portfolio"><h3>Portfolio</h3></NavLink></li>
+            <li><NavLink to="/contact"><h3>Contact</h3></NavLink></li>
           </ul>
           <div className="content">
             <Route exact path="/" component={About}/>
