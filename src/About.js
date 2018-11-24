dc3907a2ec863f34a8284792581bfbf2ab8ca7c3
Brandon Turner <brandon88@gmail.com>
2018-11-23 21:23:02 -0800

    about/resume/contact done

--- src/About.js
+++ src/About.js
@@ -3,7 +3,9 @@ class Home extends Component {
   render() {
     return (
       <div>
-
+              <p class="col"><img class="mr-2" src="./assets/images/profile.jpg" height="100" width="100" align= "left" alt="Brandon Turner"></img>
+              &nbsp;&nbsp;&nbsp;&nbsp;My name is Brandon Turner and I was born on September 2nd, 1988. I have been interested in computers since I was a child using old parts from my Fathers work as toys. The computer chips looked like a little city. I started building my own computers when I was 12 and got into video editing shortly after. From video editing I got into 2D effects. After that I moved on to 3D effects. After that I got into web development creating my first working page in 2000. I sold my first website in 2004 and from then on I was hooked.
+              </p>
       </div>
     );
   }
