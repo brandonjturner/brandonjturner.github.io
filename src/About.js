91e4d73aa3d69108ae21940acc96e9277d05ff2a
Brandon Turner <brandon88@gmail.com>
2018-11-25 16:24:55 -0800

    update about

--- src/About.js
+++ src/About.js
@@ -3,10 +3,10 @@ class Home extends Component {
   render() {
     return (
       <div>
-              <p class="col"><img class="mr-2" src="./assets/images/profile.jpg" height="100" width="100" align= "left" alt="Brandon Turner"></img>
-              &nbsp;&nbsp;&nbsp;&nbsp;My name is Brandon Turner and I have been interested in computers since I was a child using old parts from my Fathers work as toys. I started building my own computers when I was 12 and got into video editing shortly after. From video editing I got into 2D effects then to 3D effects. After that I got into web development creating my first working page in 2003. I sold my first website in 2004 and from then on I was hooked.
-              </p>
-
+        <img src="./assets/images/profile.jpg" padding-right="5px" align="left" height="100" width="100" alt="Brandon Turner">
+        </img>
+        <h3>&nbsp;&nbsp;&nbsp;&nbsp;My name is Brandon and I enjoy coding, trying new restaurants and fast cars.  I took my first programming class in 2004. I started with Visual Basic and HTML/CSS/Javascript.  I have worked on JDPower, rehabilitation and non-profit sites in my past.  I also have multiple personal projects displaying a full stack engineering skill set.
+        </h3>
       </div>
     );
   }
