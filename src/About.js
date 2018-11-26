707383d337c411dea185f3605d5f1e8f8c02b3ee
Brandon Turner <brandon88@gmail.com>
2018-11-25 18:44:10 -0800

    content push

--- src/About.js
+++ src/About.js
@@ -3,9 +3,11 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <img src="./assets/images/profile.jpg" padding-right="5px" align="left" height="100" width="100" alt="Brandon Turner">
+        <div name="profile">
+        <img src="./assets/images/profile.jpg" align="left" height="100" width="100" alt="Brandon Turner">
         </img>
-        <h3>&nbsp;&nbsp;&nbsp;&nbsp;My name is Brandon and I enjoy coding, trying new restaurants and fast cars.  I took my first programming class in 2004. I started with Visual Basic and HTML/CSS/Javascript.  I have worked on JDPower, rehabilitation and non-profit sites in my past.  I also have multiple personal projects displaying a full stack engineering skill set.
+        </div>
+        <h3>&nbsp;&nbsp;&nbsp;&nbsp;My name is Brandon and I enjoy coding, trying new restaurants and fast cars.  I took my first programming class in 2004. I started with Visual Basic and HTML/CSS/Javascript.  I have worked on JDPowers, rehabilitation and non-profit sites in my past.  I also have multiple personal projects displaying a full stack engineering skill set.
         </h3>
       </div>
     );
